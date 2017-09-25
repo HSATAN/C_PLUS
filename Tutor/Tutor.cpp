342745bdd8af9b3ec081d1aba6f4f1d36b7d1727
edison <2499090390@qq.com>
2017-09-25 23:22:01 +0800

    修改平衡因子

--- Tutor/Tutor.cpp
+++ Tutor/Tutor.cpp
@@ -1,114 +1,253 @@
-// Tutor.cpp : 定义控制台应用程序的入口点。
-//
-
 #include "stdafx.h"
-#include<iostream>
-#include<fstream>
-#include<ctime>
-#include<vector>
-#include"tree.h"
-#include<string>
-#include"split.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <csignal>
+#include <fstream>
+#include <sstream>
 using namespace std;
-double vals[] = { 10.1, 12.6, 33.1, 24.1, 50.0 };
-
- class Box
-{
-	int a = 999;
-	static Box box;
-	Box() {}
-
-public:
-
-	static int b;
-	friend void printA(Box box);
-	double length;
-	double width;
-	double height;
-	static Box get_instance();
-
+typedef int DATATYPE;
+void signalHandler(int);
+int money;
+struct thread_data {
+	int thread_id;
+	char * message;
 };
-Box Box::box = Box();
- Box  Box::get_instance()
+typedef struct BTree
 {
-	return box;
+	struct BTree * left;
+	struct BTree * right;
+	DATATYPE key;
+	int balance;
+} BT;
 
+BT * createTree(DATATYPE data, BT *tree)
+{
+	BT *node = new BT;
+
+
+	//node = (BT*)malloc(sizeof(struct BTree));
+
+	node->key = data;
+	node->left = nullptr;
+	node->right = nullptr;
+	node->balance = 0;
+	tree = node;
+	return tree;
+}
+BT * init(BT *tree, DATATYPE data)
+{
+	BT *node = new BT;
+
+
+	//node = (BT*)malloc(sizeof(struct BTree));
+
+	node->key = -1;
+	node->left = nullptr;
+	node->right = nullptr;
+	node->balance = 0;
+	tree = node;
+	return tree;
+}
+int addNode(DATATYPE data, BT *tree)
+{
+	if (!tree)
+	{
+		return 0;
 	}
 
-double & getRandom(int i) {
-
-	return vals[i];
-}
-void printA(Box box)
-{
-	cout << box.a << endl;
-}
-template <class T>
-class Stack {
-private:
-	vector<T> elems;
-public:
-	void push(T const&);
-	void pop();
-	T top() const;
-	bool empty() const
+	if (tree->key == -1 && tree->left == nullptr)
 	{
-		return elems.empty();
+		BT * node;
+		node = new BT;
+		node->key = data;
+		node->right = nullptr;
+		node->left = nullptr;
+		node->balance = 0;
+		tree->left = node;
+		return 0;
 	}
-	vector<T> get_stack()
+	if (tree->key == -1)
 	{
+		int balance = addNode(data, tree->left);
+		return balance;
+	}
+	else if (tree->right == nullptr && tree->left == nullptr)
+	{
+		BT * node;
+		node = new BT;
+		node->key = data;
+		node->right = nullptr;
+		node->left = nullptr;
+		node->balance = 0;
+		if (tree->key>data)
+		{
+
+
+			tree->left = node;
+			tree->balance += 1;
+			return 1;
+		}
+		else {
+			tree->right = node;
+			tree->balance -= 1;
+			return -1;
+		}
+
+	}
+	else if (tree->right == nullptr)
+	{
+		if (tree->key<data)
+		{
+			BT * node;
+			node = new BT;
+			node->key = data;
+			node->right = nullptr;
+			node->left = nullptr;
+			node->balance = 0;
+			tree->right = node;
+			tree->balance -= 1;
+			return 0;
+		}
+		else {
+			int balance = addNode(data, tree->left);
+			if (balance != 0)
+			{
+				tree->balance += 1;
+				return 1;
+			}
+			return 0;
+		}
+	}
+	else if (tree->left == nullptr) {
+		if (tree->key >= data)
+		{
+			BT * node;
+			node = new BT;
+			node->key = data;
+			node->right = nullptr;
+			node->left = nullptr;
+			node->balance = 0;
+			tree->left = node;
+			tree->balance += 1;
+
+		}
+		else
+		{
+			int balance = addNode(data, tree->right);
+			if (balance != 0)
+			{
+				tree->balance += -1;
+				return 0;
+			}
+		}
+	}
+	else {
+		if (tree->key>data)
+		{
+			int balance = addNode(data, tree->left);
+			if (balance != 0)
+			{
+				tree->balance += 1;
+				return 1;
+			}
+
+			return 0;
+		}
+		else
+		{
+			int balance = addNode(data, tree->right);
+			if (balance != 0)
+			{
+				tree->balance += -1;
+				return -1;
+			}
+			return 0;
+		}
+	}
+
+}
+vector<string> split(string name, string delim)
+{
+	vector<string> elems;
+	int pos = 0;
+	int end_index = 0;
+	string temp;
+	if (name.empty())
 		return elems;
-	}
-};
-
-template <class T>
-void Stack<T>::push(T const& elem)
-{
-	elems.push_back(elem);
-}
-template <class T> void Stack<T>::pop() {
-	if (elems.empty())
+	while (pos >= 0)
 	{
-		throw out_of_range("栈为空");
+		pos = (int)name.find(" ", end_index);
 
+		if (pos < 0)
+		{
+			temp = name.substr(end_index);
+			break;
+
+		}
+		temp = name.substr(end_index, pos - end_index);
+		end_index = pos + 1;
+		elems.push_back(temp);
 	}
-}
-template <class T> T Stack<T>::top() const
-{
-	if (elems.empty())
+	if (end_index != 0 && end_index < name.length())
 	{
-		throw out_of_range("空栈");
+		elems.push_back(temp);
 	}
-	return elems.back();
+	if (end_index == 0)
+	{
+		elems.push_back(name);
+	}
+	return elems;
 }
-void create_file()
+void create_tree_from_file(BT * tree)
 {
-	ofstream out;
-	out.open("test.txt");
-	out << "黄开杰 熊麟茹" << endl;
-	out.close();
-}
-void out_file()
-{
-	ifstream in;
-	in.open("test.txt");
+	ifstream in("test.txt");
 	string line;
+	vector<string> name;
 	while (getline(in, line))
 	{
-		cout << line.length()<< endl;
-		cout << line.size() << endl;
-		cout << line.substr(0, line.find(' ')) << endl;
-	
+		name = split(line, " ");
+		while (!name.empty())
+		{
+
+			int balance = addNode(stoi(name.back()), tree);
+			tree->balance += balance;
+			name.pop_back();
+		}
+	}
+	in.close();
+}
+
+void print_tree(BT * tree)
+{
+	if (tree)
+	{
+		cout << tree->key << endl;
+	}
+	if (tree->left)
+	{
+		print_tree(tree->left);
+	}
+	if (tree->right)
+	{
+		print_tree(tree->right);
 	}
 }
-
-
-int main()
+int get_depth(BT* tree)
 {
-	BTree tree=NULL;
-	tree = init_tree(999, tree);
-	string name = "黄开杰 熊麟茹 test";
-	create_tree_from_file(tree,"");
-    return 0;
+	if (!tree)
+	{
+		return 0;
+	}
+	int left = get_depth(tree->left) + 1;
+	int right = get_depth(tree->right) + 1;
+	return left>right ? left : right;
 }
-
+int main() {
+	BT *tree = NULL;
+	tree=init(tree, -1);
+	create_tree_from_file(tree);
+	cout << get_depth(tree->left) << endl;
+	print_tree(tree->left);
+	return 0;
+}
